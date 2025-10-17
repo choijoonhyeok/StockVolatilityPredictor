d01ced04d8de8e9040ee01af96e331ee1a7d543b
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2025-10-17 11:53:03 +0900

    Add database dump

--- db_dump.sql
+++ db_dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bfa37d438693b08c7a8962543d1ac73ab5d3d2fea016d5ff6bc74c54c5e488a
-size 64344062
+oid sha256:53a835f5ec5b3daba16898321708394ba52b789b6e7b8f58551a8b89b1228870
+size 29734630
